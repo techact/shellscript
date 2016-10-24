78cf88f3d5d791120b8484addfe8611cd56195a1
Suresh <suresh@ventunotech.com>
2016-10-24 11:58:50 +0530

    Removed echo's

--- find_runlevel.sh
+++ find_runlevel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # This script find run levels of services. 
 # Usage oof this script
 #chmod +x /path/to/find_runlevel.sh
@@ -11,7 +10,6 @@ clear
 echo -en "\nEnter service name to find runlevel : " 
 read service_name
 file_exists=$(ls /etc/rc*.d/*${service_name} | grep $service_name | wc -l)
-echo $file_exists
 if [[ ! $file_exists -gt 0 ]]; then
    echo "No"
    exit 0
@@ -24,12 +22,10 @@ for i in {0..6}
 do
    out=$(ls /etc/rc${i}.d/* | grep  "$service_name")
    s_name=$(echo $out | awk -F'/' '{print $4}')
-   echo $s_name
    pri=${s_name:1:2}
    statt=$(echo $s_name | awk '{print substr ($0, 0,2)}' | tr -d [0-9])
    if [[ "$statt" == "S" ]]; then
       staa="on" 
-      
       new="${new} ${i}:${staa}    "
       start_pri=$pri
    else 
@@ -41,5 +37,3 @@ done
 echo -e "\n$new"
 echo -e "\n${GREEN}Start Priority : ${start_pri} ${COFF}"
 echo -e "${RED}Stop Priority  : $stop_pri ${COFF}\n\n"
-
-#echo "$(tput setaf $i)This is ($i) $(tput sgr0)";
